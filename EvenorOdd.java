e018cfbfe28f0ce8399709c11701a6299beadfba
pranaliritgithub <patilpranu003@gmail.com>
2024-08-28 18:13:08 +0530

    Add files via upload

---
+++ EvenorOdd.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class EvenorOdd {
+    public static void main(String[]args)
+    {
+        int num=11;
+
+        if(num % 2 == 0)
+        {
+            System.out.println("Enter number is Even");
+        }
+        else{
+            System.out.println("Enter number is Odd");
+        }
+
+    }
+}
+    
+
