bef34cde92539001415d42c7da4a822a0774288d
pranaliritgithub <patilpranu003@gmail.com>
2024-08-29 18:01:17 +0530

    Add files via upload

---
+++ Fahrenheittocelsius.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Fahrenheittocelsius {
+    public static void main(String[]args){
+
+     double fahrenheit = 98.6, celsius = 0.0;
+
+        celsius = ((fahrenheit -32)*5)/9;
+
+        System.out.println("temperature in celsius:" + celsius);
+    }
+}
+
