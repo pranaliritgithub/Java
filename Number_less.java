ed16e2ddb81206b6f59630f2fc0d650d28c5fc4d
pranaliritgithub <patilpranu003@gmail.com>
2024-08-29 19:55:28 +0530

    Add files via upload

---
+++ Number_less.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class Number_less 
+{
+    public static void main(String[]args)
+    {
+        int Number = 10;
+
+        Scanner sc = new Scanner(System.in);
+
+       
+    
+        if(Number==1 || Number<=10){
+            System.out.println("Hello");
+
+        }
+        else{
+            System.out.println("World");
+        }
+
+
+        }
+    }
+
+    
+
+
+    
+
