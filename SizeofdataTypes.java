4c497a7add0ca329223120e6a029758517e5c4db
pranaliritgithub <patilpranu003@gmail.com>
2024-08-29 17:59:08 +0530

    Add files via upload

---
+++ SizeofdataTypes.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class SizeofdataTypes
+{
+    public static void main(String[]args){
+        System.out.println("Size of int: " + (Integer.SIZE/8) + " bytes.");
+        System.out.println("Size of float: " + (Float.SIZE/8) + " bytes.");
+        System.out.println("Size of double: " + (Double.SIZE/8) + " bytes.");
+
+
+
+    }
+}
