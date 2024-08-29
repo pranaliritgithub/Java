b14f926eb73eb7aa42ddb2818b96f76400e3a32b
pranaliritgithub <patilpranu003@gmail.com>
2024-08-29 17:59:42 +0530

    Add files via upload

---
+++ CalculateArea.java
@@ -0,0 +1,11 @@
+public class CalculateArea {
+    public static void main(String[]args){
+
+        double width = 7.5,area;
+        double Height = 4.2;
+
+        area =width*Height;
+        System.out.println("calculate area is : " + area);
+
+    }
+}
