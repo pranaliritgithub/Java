ccec985e2850faba55e26d8cd7cf440adf4915ee
pranaliritgithub <patilpranu003@gmail.com>
2024-08-29 18:01:41 +0530

    Add files via upload

---
+++ PositiveOrNegative.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class PositiveOrNegative {
+    public static void main(String[]args){
+        
+      int num = 27;
+        
+             if(num < 0){
+             System.out.println(num + " is Positive.");
+            }
+             else{
+                 System.out.println(num + " is Negative.");
+                    
+                }
+                }
+        }
+        
+        
