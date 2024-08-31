000da4ce3788c6c4a85890887e0e5c371994be69
pranaliritgithub <patilpranu003@gmail.com>
2024-08-31 15:19:07 +0530

    Add files via upload

---
+++ Loop.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Loop {
+    public static void main(String[]args){
+        int i= 101;
+        
+        Scanner sc=new Scanner(System.in);
+        
+        for(i=101;i<=110;i++)
+        {
+            System.out.println(i);        }
+    }
+    
+}
