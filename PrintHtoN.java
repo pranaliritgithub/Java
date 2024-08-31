fa8bb4e6e68660d587b1433fd71ea3b523df7e7b
pranaliritgithub <patilpranu003@gmail.com>
2024-08-31 15:15:16 +0530

    Add files via upload

---
+++ PrintHtoN.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class PrintHtoN {
+    public static void main(String[]args){
+
+     char i = 'H';
+     
+        Scanner sc = new Scanner(System.in);
+
+        for(i='H';i<='N';i++){
+            System.out.println(i);
+
+        }
+
+        
+    }
+    
+}
