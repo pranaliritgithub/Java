5b8200e12201322765104217ec4ed86a642ee690
pranaliritgithub <patilpranu003@gmail.com>
2024-08-31 15:20:28 +0530

    Add files via upload

---
+++ Vowel.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Vowel {
+    public static void main(String[]args){
+
+       char char1 = 'a';
+       char char2 = 'e';
+       char char3 = 'i';
+       char char4 = 'o';
+       char char5= 'u';
+
+       Scanner sc = new Scanner(System.in);
+
+       if( char5<='p'){
+        System.out.println("True");
+       }
+       else{
+        System.out.println("False");
+       }
+    }
+    
+}
