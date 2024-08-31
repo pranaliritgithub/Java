e55a68cc27f73422e13f815f1711a18d5129b1f2
pranaliritgithub <patilpranu003@gmail.com>
2024-08-31 15:14:35 +0530

    Add files via upload

---
+++ PrintNo.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class PrintNo {
+    public static void main(String[]args){
+
+        int i = 10;
+
+        Scanner sc =new Scanner(System.in);
+
+        for(i=10;i>=1;i--){
+            System.out.println(i);
+        }
+    }
+    
+}
