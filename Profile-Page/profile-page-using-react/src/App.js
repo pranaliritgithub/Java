9303df3cc73c4f56ee0de28917c8756eb1337748
pranaliritgithub <patilpranu003@gmail.com>
2024-11-11 22:35:49 +0530

    Add files via upload

---
+++ Profile-Page/profile-page-using-react/src/App.js
@@ -0,0 +1,12 @@
+import react from 'react';
+import ProfilePage from './components/ProfilePage';
+
+function App() {
+  return (
+    <div className="App">
+      <ProfilePage/>
+</div>
+  );
+}
+
+export default App;
